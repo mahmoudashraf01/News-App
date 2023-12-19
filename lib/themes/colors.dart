8ce589e3dab32c97b6c18e18b3c67994dfe716ee
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2023-12-19 20:57:45 +0200

    add app bar

---
+++ lib/themes/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color tealColor = Colors.teal;
+const Color white = Colors.white;
+const Color white2 = Color.fromARGB(255, 169, 168, 168);
+const Color black = Colors.black;
+const Color black2 = Color.fromARGB(255, 26, 26, 26);
+const Color grey = Color.fromARGB(255, 144, 144, 144);
+const Color darkWhite = Color.fromARGB(255, 207, 210, 207);
+const Color darkGreen = Color.fromARGB(255, 0, 68, 62);
+const Color appColor = Color.fromRGBO(1, 34, 43, 1);
+const Color appColor2 = Color.fromARGB(191, 0, 21, 27);
+const Color darkYellow = Color.fromARGB(255, 250, 136, 7);
+const Color orange = Color.fromARGB(255, 232, 93, 4);
+const Color ligthRed = Color.fromARGB(255, 174, 7, 7);
+const Color transparent =  Colors.transparent;
+
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:whether_api/themes/colors.dart';
+import 'package:whether_api/themes/text.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'News',
+              style: h5Bold,
+            ),
+            Text(
+              'Cloud',
+              style: h5Bold.merge(
+                const TextStyle(color: orange),
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      body: const Column(),
+    );
   }
 }
